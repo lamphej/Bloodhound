a35eddee12590abce241002cfe2b24ebdc154ae6
lamphej <james@jameslamphere.com>
2015-10-03 15:53:26 -0400

    Good update

--- Monitor/main.py
+++ Monitor/main.py
@@ -4,6 +4,13 @@ from scapy.all import *
 import pprint
 import requests
 
+def insert_new_ip(src_ip):
+    packet_data[src_ip] = {}
+    packet_data[src_ip]["count"] = 0
+    packet_data[src_ip]["incoming"] = 0
+    packet_data[src_ip]["outgoing"] = 0
+
+
 def customAction(packet):
     global packetCount, my_macs, packet_data, monitor_config
     packetCount += 1
@@ -12,26 +19,17 @@ def customAction(packet):
         src_ip = packet[0][1].src
         dst_ip = packet[0][1].dst
         src_mac = packet[Ether].src
-        if not src_ip == monitor_config["ip"]:
+        if not src_ip == our_ip:
             inbound = True
             if not packet_data.has_key(src_ip):
-                packet_data[src_ip] = {}
-                packet_data[src_ip]["count"] = 0
-                packet_data[src_ip]["incoming"] = 0
-                packet_data[src_ip]["outgoing"] = 0
+                insert_new_ip(src_ip)
             packet_data[src_ip]["count"] += 1
             packet_data[src_ip]["incoming"] += 1
         else:
             if not packet_data.has_key(dst_ip):
-                packet_data[dst_ip] = {}
-                packet_data[dst_ip]["count"] = 0
-                packet_data[dst_ip]["incoming"] = 0
-                packet_data[dst_ip]["outgoing"] = 0
+                insert_new_ip(dst_ip)
             packet_data[dst_ip]["count"] += 1
             packet_data[dst_ip]["outgoing"] += 1
-        #dst_ = "Packet #%s: %s ==> %s" % (packetCount, src_ip, dst_ip)
-        dst_ = ""
-        #return dst_
     except:
         pass
 
@@ -41,8 +39,8 @@ def send_reports():
         report_url = monitor_config["report_url"]
         report_dict = {
             "src": {
-                "name": monitor_config["name"],
-                "ip": monitor_config["ip"]
+                "name": our_name,
+                "ip": our_ip
             },
             "data": [
 
@@ -60,6 +58,8 @@ def send_reports():
         time.sleep(5)
 
 if __name__ == "__main__":
+    our_name = socket.gethostname()
+    our_ip = socket.gethostbyname(socket.gethostname())
     monitor_config = json.loads(open("config.json", "r").read())
     packetCount = 0
     packet_data = {}
--- Server/server.py
+++ Server/server.py
@@ -1,36 +1,73 @@
+from datetime import datetime
 from flask import Flask, render_template, request
-import json
+import ujson
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     global network_data
-    return render_template("index.html", network_data=network_data)
+    return render_template("index.html",
+                           network_data=network_data,
+                           client_count = len(network_data.keys())
+                           )
+
+@app.route('/json_report/<host_name>')
+def json_report(host_name=None):
+    global network_data
+    ret_list = []
+    if host_name == None:
+        pass
+    else:
+        ret_list.append([
+            network_data[host_name]["time_data"][0]["time"],
+            int(network_data[host_name]["time_data"][0]["total"])
+        ])
+        for i in range(1, len(network_data[host_name]["time_data"])):
+            ret_list.append([
+                network_data[host_name]["time_data"][i]["time"],
+                int(network_data[host_name]["time_data"][i]["total"]) - sum([x[1] for x in ret_list[0:i]])
+            ])
+    return ujson.dumps(ret_list)
 
 @app.route('/report/submit', methods=["POST"])
 def submit_report():
     global network_data
-    js = json.loads(request.data)
+    js = ujson.loads(request.data)
     src_ip = js["src"]["ip"]
     src_name = js["src"]["name"]
     if not network_data.has_key(src_name):
         network_data[src_name] = {}
         network_data[src_name]["ip"] = src_ip
         network_data[src_name]["data"] = {}
+        network_data[src_name]["time_data"] = []
     dst_data = js["data"]
+    total_time_data = {
+        "time": datetime.now(),
+        "incoming": 0,
+        "outgoing": 0,
+        "total": 0
+    }
     for dst_host in dst_data:
         dst_ip = dst_host["ip"]
         if not network_data[src_name]["data"].has_key(dst_ip):
-            network_data[src_name]["data"][dst_ip] = {}
+            network_data[src_name]["data"][dst_ip] = []
         packet_counts = dst_host["packet_counts"]
         incoming_count = packet_counts["incoming"]
+        total_time_data["incoming"] += incoming_count
         outgoing_count = packet_counts["outgoing"]
+        total_time_data["outgoing"] += outgoing_count
         total_count = incoming_count + outgoing_count
-        network_data[src_name]["data"][dst_ip]["incoming"] = incoming_count
-        network_data[src_name]["data"][dst_ip]["outgoing"] = outgoing_count
-        network_data[src_name]["data"][dst_ip]["total"] = total_count
-    return json.dumps({"status": "ok"})
+        total_time_data["total"] += total_count
+        time_data = {
+            "time": datetime.now(),
+            "incoming": incoming_count,
+            "outgoing": outgoing_count,
+            "total": total_count
+        }
+        network_data[src_name]["data"][dst_ip].append(time_data)
+    network_data[src_name]["time_data"].append(total_time_data)
+    return ujson.dumps({"status": "ok"})
 
 if __name__ == '__main__':
     network_data = {}
-    app.run()
+    app.run(host="128.153.178.47")
